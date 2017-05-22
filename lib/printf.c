f66ad249d4b9b3616653be67f1166c97fb53f186
Jason Lu <jasonbangbang@gmail.com>
2017-05-21 20:28:10 -0500

    Use ui2a

--- lib/printf.c
+++ lib/printf.c
@@ -1,5 +1,4 @@
 #include <lib/printf.h>
-#include <stdlib.h>
 #include <string.h>
 
 static char *merge_strings(char *target, char *src)
@@ -8,6 +7,25 @@ static char *merge_strings(char *target, char *src)
         *target++ = *src++;
     }
     return target;
+}
+
+static void ui2a(unsigned int num, char *buffer, unsigned int base,
+                 int uppercase)
+{
+    int n = 0;
+    unsigned int d = 1;
+    while (num / d >= base)
+        d *= base;
+    while (d != 0) {
+        int dgt = num / d;
+        num %= d;
+        d /= base;
+        if (n || dgt > 0 || d == 0) {
+            *buffer++ = dgt + (dgt < 10 ? '0' : (uppercase ? 'A' : 'a') - 10);
+            ++n;
+        }
+    }
+    *buffer = 0;
 }
 
 int vsprintf(char *s, const char *format, va_list arg)
@@ -19,14 +37,18 @@ int vsprintf(char *s, const char *format, va_list arg)
             pos++;
             switch (*pos) {
                 case 'd':
-                    itoa(va_arg(arg, int), int_conv, 10);
+                    ui2a(va_arg(arg, int), int_conv, 10, 0);
                     s = merge_strings(s, int_conv);
                     break;
                 case 's':
                     s = merge_strings(s, va_arg(arg, char *));
                     break;
                 case 'x':
-                    itoa(va_arg(arg, int), int_conv, 16);
+                    ui2a(va_arg(arg, int), int_conv, 16, 0);
+                    s = merge_strings(s, int_conv);
+                    break;
+                case 'X':
+                    ui2a(va_arg(arg, int), int_conv, 16, 1);
                     s = merge_strings(s, int_conv);
                     break;
             }
