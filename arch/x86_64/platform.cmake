703a7c0a93bdf261ceaa0e30a413db9b9195166e
Jason Lu <jasonbangbang@gmail.com>
2017-05-30 21:45:09 -0500

    Use x86_64-pc-none-elf instead of x86_64-pc-linux-elf
    Using the Linux target when we clearly aren't is dangerous, even if
    it works for now. We don't know what other flags are inserted by
    default with the Linux target, so we use a generic one instead.
    
    To facilitate this, the linking action has been patched to use the linker
    instead of the system compiler, which is the default behavior when
    the target is none, not Linux.

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,7 +3,7 @@ add_definitions(
     -DX86_64
 )
 
-# Temporarily use the Linux target because otherwise Clang will pass it to GCC instead of LD
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-linux-elf -mcmodel=kernel -mno-red-zone")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld -Wl,-T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-none-elf -mcmodel=kernel -mno-red-zone")
+# Be careful of what you pass in here. Input will go directly to LLD, so no Clang flags allowed
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
 set(CMAKE_ASM_NASM_COMPILE_OBJECT "${CMAKE_ASM_NASM_COMPILE_OBJECT} -felf64")
