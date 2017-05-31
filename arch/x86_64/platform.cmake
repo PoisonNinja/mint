bf39026faf84933eb9f5acd3a057d688fbe0cb5f
Jason Lu <jasonbangbang@gmail.com>
2017-05-30 20:48:03 -0500

    Switch over to Clang/LLVM
    Compiles successfully and boots up! For now we use x86_64-pc-linux-elf,
    but in the future we should probably use x86_64-pc-none-elf to prevent
    weird stuff from happening.

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,6 +3,7 @@ add_definitions(
     -DX86_64
 )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcmodel=kernel -mno-red-zone")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -zmax-page-size=0x1000 -T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
+# Temporarily use the Linux target because otherwise Clang will pass it to GCC instead of LD
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-linux-elf -mcmodel=kernel -mno-red-zone")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=lld -Wl,-T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
 set(CMAKE_ASM_NASM_COMPILE_OBJECT "${CMAKE_ASM_NASM_COMPILE_OBJECT} -felf64")
