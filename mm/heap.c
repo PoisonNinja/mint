92ad9025586ba45335e6828a168084b8e287eded
Jason Lu <jasonbangbang@gmail.com>
2017-06-12 21:39:44 -0500

    Convert back to //

--- mm/heap.c
+++ mm/heap.c
@@ -1,7 +1,7 @@
 #include <kernel.h>
 #include <mm/heap.h>
 
-/* Variables for early heap */
+// Variables for early heap
 static addr_t early_heap_start = 0;
 static addr_t early_heap_end = 0;
 static addr_t early_heap_extent = 0;
@@ -12,17 +12,17 @@ void early_malloc_set_properties(addr_t start, addr_t extent)
 {
     if (!start || !extent)
         return;
-    /* No, I don't want to configure this twice */
+    // No, I don't want to configure this twice
     if (early_heap_configured) {
         printk(WARNING, "early_malloc: Already configured! Allowing this "
                         "action, but unexpected behavior may occur\n");
     }
-    /* Set the early heap state */
+    // Set the early heap state
     early_heap_start = start;
     early_heap_extent = extent;
-    /* Precalculate the heap end */
+    // Precalculate the heap end
     early_heap_end = start + extent;
-    /* Set the heap pointer to the start of the memory region */
+    // Set the heap pointer to the start of the memory region
     early_heap_watermark = start;
     printk(INFO, "early_malloc: Start at %p, extent is %p\n", start, extent);
     early_heap_configured = 1;
@@ -30,11 +30,11 @@ void early_malloc_set_properties(addr_t start, addr_t extent)
 
 void* __attribute__((malloc)) early_malloc(size_t size)
 {
-    /* Make sure the request doesn't overshoot the extent */
+    // Make sure we don't overshoot the extent
     if (early_heap_watermark + size > early_heap_end) {
         printk(INFO, "early_malloc: Request too large :(\n");
     }
-    /* Convert the watermark into a void pointer to return */
+    // Convert water into a void pointer to return
     void* ret = (void*)early_heap_watermark;
     early_heap_watermark += size;
     return ret;
