40bf51801eab2696bcc15c0d73c4101e60e216a3
Jason Lu <jasonbangbang@gmail.com>
2017-07-08 17:58:45 -0700

    Remove container_of macro
    No Linux code allowed, so we gotta remove this

--- include/kernel.h
+++ include/kernel.h
@@ -34,12 +34,6 @@
 #define likely(x) __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 
-#define container_of(ptr, type, member)                    \
-    ({                                                     \
-        const typeof(((type *)0)->member) *__mptr = (ptr); \
-        (type *)((char *)__mptr - offsetof(type, member)); \
-    })
-
 enum {
     DEBUG = 0,
     INFO,
