f2de3dfbc9126814742687dfbd478529bd48cf41
Jason Lu <syscallrax@gmail.com>
2017-07-22 17:23:42 -0400

    Add S_* and O_* flags

--- include/fs/fs.h
+++ include/fs/fs.h
@@ -2,6 +2,16 @@
 
 #include <types.h>
 
+#define O_RDONLY 0x0000
+#define O_WRONLY 0x0001
+#define O_RDWR 0x0002
+#define O_APPEND 0x0008
+#define O_CREAT 0x0200
+#define O_TRUNC 0x0400
+#define O_EXCL 0x0800
+#define O_NOFOLLOW 0x1000
+#define O_PATH 0x2000
+
 #define DENTRY_NAME_MAX 256
 
 struct inode;
