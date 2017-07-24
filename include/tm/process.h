4655791180c6a944db31a0802d96155193f50a8e
Jason Lu <syscallrax@gmail.com>
2017-07-24 13:48:21 -0400

    Add file table to process

--- include/tm/process.h
+++ include/tm/process.h
@@ -5,11 +5,20 @@
 #include <tm/thread.h>
 #include <types.h>
 
+#define MAX_FD 128
+
+struct files_table {
+    struct file **files;
+    uint8_t *file_bitset;
+};
+
 struct process {
     pid_t pid;
     uint32_t status;
     uint32_t flags;
     struct memory_context ctx;
+    // Filesystem accounting
+    struct files_table files;
     struct inode *root, *cwd;
     // Process tree stuff
     struct process *parent, *children;
