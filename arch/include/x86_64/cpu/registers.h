162b05f4d77af3b892ae41781fe470be3d30ee5f
Jason Lu <syscallrax@gmail.com>
2017-07-28 14:03:21 -0400

    Fix CPU registers to match push order

--- arch/include/x86_64/cpu/registers.h
+++ arch/include/x86_64/cpu/registers.h
@@ -34,8 +34,19 @@
 #include <types.h>
 
 struct registers {
-    uint64_t r15, r14, r13, r12, rbp, rbx, r11, r10, r9, r8, rax, rcx, rdx, rsi,
-        rdi;
+    uint64_t r15, r14, r13, r12, r11, r10, r9, r8, rsi, rdi, rbp, rdx, rcx, rbi,
+        rax;
     uint64_t int_no, err_code;
     uint64_t rip, cs, rflags, userrsp, ss;
 } __attribute__((packed));
+
+struct context {
+    uintp r15;
+    uintp r14;
+    uintp r13;
+    uintp r12;
+    uintp r11;
+    uintp rbx;
+    uintp ebp;  // rbp
+    uintp eip;  // rip;
+};
