79674d8367918d80173b42f47dff6d80b2ae77df
Jason Lu <syscallrax@gmail.com>
2017-08-03 17:12:45 -0500

    Drop -fno-omit-frame-pointer
    We'll use DWARF CFI unwinding information to do this

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,7 +3,7 @@ add_definitions(
     -DX86_64
 )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-none-elf -fno-omit-frame-pointer -mcmodel=kernel -mno-red-zone -O3")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-none-elf -mcmodel=kernel -mno-red-zone -O3")
 
 # Be careful of what you pass in here. Input will go directly to LLD, so no Clang flags allowed
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
