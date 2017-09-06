421416a7150541c240af3426bc9d1b4874dacba9
Jason Lu <syscallrax@gmail.com>
2017-09-05 20:22:28 -0500

    Zero out DMA memory

--- mm/dma.c
+++ mm/dma.c
@@ -15,6 +15,7 @@ struct dma_region* dma_alloc(size_t size)
     addr_t virtual = valloc_alloc(dma_region, size);
     virtual_map(&kernel_context, virtual, physical, size,
                 PAGE_PRESENT | PAGE_WRITABLE);
+    memset((void*)virtual, 0, size);
     struct dma_region* region = kmalloc(sizeof(struct dma_region));
     region->physical_base = physical;
     region->virtual_base = virtual;
