31e850bb07e74af12313aecc10ee70e52407be87
Jason Lu <syscallrax@gmail.com>
2017-09-25 19:39:06 -0500

    ahci: Move initialization to DEVICE_INITCALL

--- drivers/ata/ahci/ahci.c
+++ drivers/ata/ahci/ahci.c
@@ -363,11 +363,16 @@ static int ahci_probe(struct pci_device* device)
 }
 
 static struct pci_device_filter ahci_device_filter = {
-    .vendor_id = 0, .device_id = 0, .class = 1, .subclass = 6,
+    .vendor_id = 0,
+    .device_id = 0,
+    .class = 1,
+    .subclass = 6,
 };
 
 static struct pci_driver ahci_driver = {
-    .name = "ahci", .filter = &ahci_device_filter, .probe = ahci_probe,
+    .name = "ahci",
+    .filter = &ahci_device_filter,
+    .probe = ahci_probe,
 };
 
 static int init_ahci(void)
@@ -375,4 +380,4 @@ static int init_ahci(void)
     pci_driver_register(&ahci_driver);
     return 0;
 }
-TEST_INITCALL(init_ahci);
+DEVICE_INITCALL(init_ahci);
