a10a753dcd30911985255299142a849505f94e71
Jason Lu <syscallrax@gmail.com>
2018-11-30 13:07:30 -0600

    Fixes to build using Pepper GCC toolchain

--- arch/x86_64/platform.cmake
+++ arch/x86_64/platform.cmake
@@ -3,9 +3,9 @@ add_definitions(
     -DX86_64
 )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --target=x86_64-pc-none-elf -mcmodel=kernel -mno-red-zone")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcmodel=kernel -mno-red-zone")
 
 # Be careful of what you pass in here. Input will go directly to LLD, so no Clang flags allowed
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -zmax-page-size=0x1000 -T${CMAKE_SOURCE_DIR}/arch/x86_64/mint.lds")
 
-set(CMAKE_ASM_NASM_COMPILE_OBJECT "${CMAKE_ASM_NASM_COMPILE_OBJECT} -felf64")
+set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
